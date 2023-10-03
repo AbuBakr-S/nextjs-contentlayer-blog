1a7633ce23153dbb1c3dc9ea06a6e3057d2f1064
Abubakar Siddique <abz.siddique@gmail.com>
2023-10-03 14:10:18 +0100

    style: add indentation

--- src/app/layout.js
+++ src/app/layout.js
@@ -56,11 +56,11 @@ export default function RootLayout({ children }) {
         )} 
       >
         <Script id="theme-switcher">
-        {`if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark')
-          } else {
-            document.documentElement.classList.remove('dark')
-        }`}
+          {`if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+              document.documentElement.classList.add('dark')
+            } else {
+              document.documentElement.classList.remove('dark')
+          }`}
         </Script>
         <Header />
         {children}

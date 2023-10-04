f7b445664fe67c5559f4cce7576b3d712e05c10f
Abubakar Siddique <abz.siddique@gmail.com>
2023-10-04 16:11:32 +0100

    feat: make header responsive

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -5,11 +5,11 @@ import profileImage from '@/public/profile-img.png'
 
 const Logo = () => {
   return (
-    <Link className="flex items-center text-dark gap-4" href="/">
-      <div className="w-16 rounded-full overflow-hidden border border-solid border-dark">
+    <Link className="flex items-center text-dark gap-4 dark:text-light" href="/">
+      <div className="w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
         <Image src={profileImage} alt="Abubakar" className="w-full h-auto rounded-full" />
       </div>
-      <span className="font-bold text-xl">Abubakar Siddique</span>
+      <span className="font-bold dark:font-semibold text-lg md:text-xl">Abubakar Siddique</span>
     </Link>
   )
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,70 @@
 "use client"
-import React from 'react'
 import Logo from './Logo'
 import Link from 'next/link'
-import { GithubIcon, LinkedInIcon, SunIcon, TwitterIcon } from '../Icons'
+import { GithubIcon, LinkedInIcon, SunIcon, MoonIcon, TwitterIcon } from '../Icons'
 import siteMetadata from '../../utils/siteMetaData'
 import { useThemeSwitch } from '../Hooks/useThemeSwitch'
+import { useState } from 'react'
+import { cx } from '../../utils/helpers'
 
 export const Header = () => {
   const [theme, setTheme] = useThemeSwitch();
+  const [click, setClick] = useState();
+
+  const toggle = () => setClick(!click);
+
   return (
-    <div className="w-full p-4 px-10 flex items-center justify-between">
+    <div className="w-full px-5 sm:px-10 flex items-center justify-between">
       <Logo />
-      <nav className="w-max py-3 px-8 border border-solid rounded-full font-medium capitalize flex items-center 
+      <button className="inline-block sm:hidden z-50" onClick={toggle} aria-label="Hamburger Menu">
+        <div className="w-6 cursor-pointer transition-all ease duration-300">
+          <div className="relative">
+            <span className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
+              style={{
+                transform: click ? "rotate(-45deg) translateY(0)" : "rotate(0deg) translateY(6px)"
+                }}
+            >
+              &nbsp;
+            </span>
+            <span className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
+              style={{
+                opacity: click ? 0 : 1
+              }}
+            >
+              &nbsp;
+            </span>
+            <span className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
+              style={{
+                transform: click ? "rotate(45deg) translateY(0)" : "rotate(0deg) translateY(-6px)"
+              }}
+            >&nbsp;</span>
+          </div>
+        </div>
+      </button>
+      {/* Mobile Navigation */}
+      <nav className=" w-max py-3 px-6 sm:px-8 border border-solid border-dark rounded-full font-medium capitalize  items-center flex  sm:hidden
+        fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50
+        transition-all ease duration-300
+        "
+        style={{
+          top: click ? "1rem" : "-5rem"
+        }}
+      >
+        <Link href="/" className="mr-2">Home</Link>
+        <Link href="/about" className="mx-2">About</Link>
+        <Link href="/contact" className="mx-2">Contact</Link>
+        <button onClick={() => setTheme(theme === "light" ? "dark" : "light")  }
+        className={cx("w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1", theme === "light" ? "bg-dark text-light" :
+        "bg-light text-dark" )}
+        aria-label="theme-switcher"
+        >
+          {
+            theme === "light" ? <MoonIcon className={"fill-dark"} />  : <SunIcon className={"fill-dark"} />
+          }
+        </button>
+      </nav>
+      {/* Navigation */}
+      <nav className="w-max py-3 px-8 border border-solid rounded-full font-medium capitalize items-center hidden sm:flex
       fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50">
         <Link className="mr-2" href="/">Home</Link>
         <Link className="mx-2" href="/about">About</Link>
@@ -20,11 +73,11 @@ export const Header = () => {
           <SunIcon />
         </button>
       </nav>
-      <div>
+      <div className="hidden sm:flex items-center">
         {/* External links */}
         <a className="inline-block w-6 h-6 mr-4" href={siteMetadata.linkedin}><LinkedInIcon className="hover:scale-125 transition-all ease duration-200" /></a>
-        <a className="inline-block w-6 h-6 mr-4" href={siteMetadata.twitter}><TwitterIcon className="hover:scale-125 transition-all ease duration-200" /></a>
-        <a className="inline-block w-6 h-6 mr-4" href={siteMetadata.github}><GithubIcon className="hover:scale-125 transition-all ease duration-200" /></a>
+        <a className="inline-block w-6 h-6 mr-4" href={siteMetadata.twitter}><TwitterIcon className="hover:scale-125 transition-all ease duration-200 dark:bg-light" /></a>
+        <a className="inline-block w-6 h-6 mr-4" href={siteMetadata.github}><GithubIcon className="hover:scale-125 transition-all ease duration-200 dark:fill-light" /></a>
       </div>
     </div>
   )

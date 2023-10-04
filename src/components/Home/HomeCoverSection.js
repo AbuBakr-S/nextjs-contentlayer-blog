01e9fe0f8fe7d1866007fac6e7efd51cc8235ffe
Abubakar Siddique <abz.siddique@gmail.com>
2023-10-04 16:28:22 +0100

    feat: make home cover section responsive

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -10,7 +10,7 @@ const HomeCoverSection = ({ blogs }) => {
   const blog = sortedBlogs[0];
   return (
     <div className="w-full inline-block">
-      <article className="flex flex-col items-start justify-end mx-10 relative h-[85vh]">
+      <article className="flex flex-col items-start justify-end mx-5 sm:mx-10 relative h-[60vh] sm:h-[85vh]">
         <div className="absolute top-0 left-0 bottom-0 right-0 h-full bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-0"></div>
         <Image
           className="w-full h-full object-center object-cover rounded-3xl -z-10"
@@ -20,10 +20,10 @@ const HomeCoverSection = ({ blogs }) => {
           alt={blog.title}
           fill
         />
-        <div className="w-3/4 flex flex-col items-start p-16 justify-center z-0 text-light">
+        <div className="w-full lg:w-3/4 p-6 sm:p-8 md:p-12 lg:p-16 flex flex-col items-start justify-center z-0 text-light">
           <Tag link={`categories/${slug(blog.tags[0])}`} name={blog.tags[0]} />
           <Link className="mt-6" href={blog.url}>
-            <h1 className="font-bold capitalize text-4xl">
+            <h1 className="font-bold capitalize text-lg sm:text-xl md:text-3xl lg:text-4xl">
               <span className="bg-gradient-to-r from-accent to-accent dark:from-accentDark/50 
                 dark:to-accentDark/50 bg-[length:0px_6px]
                 hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500">

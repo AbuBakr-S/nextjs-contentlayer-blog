0de0a29c484a6cb1a7ebd9ef077048de5781d29f
Abubakar Siddique <abz.siddique@gmail.com>
2023-10-05 14:01:32 +0100

    feat: make about page responsive

--- src/app/(about)/about/page.js
+++ src/app/(about)/about/page.js
@@ -14,8 +14,8 @@ const About = () => {
     <>
       <AboutCoverSection />
       <Skills />
-      <h3 className="mt-8 font-semibold text-lg md:text-1xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal"> 
-      Have a project in mind? Reach out to me 📞 from <Link href="/contact"  className="!underline underline-offset-4"   >here</Link> and let's make it happen.
+      <h3 className="mt-8 font-semibold text-lg md:text-xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal"> 
+      Have a project in mind? Reach out to me 📞 from <Link href="/contact" className="!underline underline-offset-4">here</Link> and let's make it happen.
       </h3>
     </>
   )
--- src/components/About/Skills.js
+++ src/components/About/Skills.js
@@ -16,12 +16,12 @@ const Skills = () => {
   return (
     <section className="w-full flex flex-col p-5 xs:p-10 sm:p-12 md:p-16 lg:p-20 border-b-2 border-solid border-dark dark:border-light
     text-dark dark:text-light">
-      <h3 className="text-3xl font-semibold text-accent">My Skills</h3>
-      <ul className="flex flex-wrap gap-5 mt-8">
+      <h3 className="sm:text-3xl md:text-4xl font-semibold text-accent dark:text-accentDark">My Skills</h3>
+      <ul className="flex flex-wrap gap-5 mt-8 justify-center xs:justify-start">
         {SkillList.map((skill, index) => (
           <li 
             key={index}
-            className="font-semibold border w-fit px-5 py-4 capitalize text-base hover:scale-105 transition-all ease duration-200 cursor-pointer"
+            className="font-semibold inline-block capitalize text-base xs:text-sm md:text-lg py-2 xs:py-3 sm:py-4 lg:py-4 px-4 xs:px-6 sm:px-8 lg:px-12 border-2 border-solid border-dark dark:border-light rounded mr-3 mb-3 xs:mr-4 xs:mb-4  md:mr-6 md:mb-6 hover:scale-105 transition-all ease duration-200 cursor-pointer dark:font-normal"
           >
             {skill}
           </li>
